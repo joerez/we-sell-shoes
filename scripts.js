9043d263064b9aa87661b6c79c54d39d8458c191
Joerez <rezenbiz@gmail.com>
2018-07-30 11:37:39 -0700

    added some extra stuff

--- scripts.js
+++ scripts.js
@@ -1,46 +1,15 @@
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-
-let slide1 = document.querySelector('.slide1');
-let slide2 = document.querySelector('.slide2');
-
-let hero = document.querySelector('.hero');
+let popUp = document.getElementById('pop-up');
+let popUpLayer = document.getElementById('pop-up-layer');
+let closeModal = document.getElementById('close-pop-up');
 
 
-next.addEventListener("click", function() {
-
-
-  if (hero.classList.contains('prev')) {
-    hero.classList.remove('prev');
-  }
-  window.clearInterval(nextint);
-  window.clearInterval(prevint);
-  hero.classList.add('next');
-
+closeModal.addEventListener("click", function() {
+  popUpLayer.style.display = 'none';
 })
 
-prev.addEventListener("click", function() {
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
 
-  if (hero.classList.contains('next')) {
-    hero.classList.remove('next');
-  }
+  window.clearInterval(popUpFun);
 
-  window.clearInterval(nextint);
-  window.clearInterval(prevint);
-  hero.classList.add('prev');
-})
-
-let nextint = window.setInterval(function() {
-  if (hero.classList.contains('prev')) {
-    hero.classList.remove('prev');
-    hero.classList.add('next');
-  }
-
-}, 4000)
-
-let prevint = window.setInterval(function() {
-  if (hero.classList.contains('next')) {
-    hero.classList.remove('next');
-    hero.classList.add('prev');
-  }
-}, 8000)
+}, 10 * 1000)
